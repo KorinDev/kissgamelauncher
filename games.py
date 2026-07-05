06b525051e541dfe546b247db478f169197e3f61
Korin <korin@klc.my.domain>
2026-07-05 10:44:24 +0200

    Added JSON config + misc. bugfixes and error handling.

--- games.py
+++ games.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 import os
-games = {
-    "doom": "crispy-doom -iwad /home/korin/Games/DOOM/doom1.wad"
-}
+import json
+from pathlib import Path
+
+CONFIG_PATH = Path.home() / ".config/kiss/games.json"
+
+def load_games():
+    if not CONFIG_PATH.exists():
+        print("KISS: Game config file ~/.config/kiss/games.json missing.")
+        quit()
+    with open(CONFIG_PATH) as f:
+        data = json.load(f)
+    return data
 
 def main():
-    global games
+    games = load_games()
     print("KISS Game Launcher")
-    print("*"*18)
-    for key in games.keys():
-        print(f"- ] {key}")
-        print("*"*18)
-        x = input("- ? ")
+    print("*"*32)
+    for key, value in games.items():
+        print(f"- ] <{key}>\t\t\"{value['name']}\"")
+    print("*"*32)
+    x = input("- ? ")
+    if not x in games:
+        print(f"KISS: Game '{x}' not found.")
+        quit()
     try:
-        gameval = games.get(x)
+        gameval = games[x]["cmd"]
+
         os.system(gameval)
     except Exception as e:
-        print("*"*18)
+        print("ERR: " + e)
         
 
 
